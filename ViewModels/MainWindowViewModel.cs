10e3f66be50f4d472c77c4e92ac8b1995775fd99
Дмитрий Шошин < @DESKTOP-Q7FEKIF>
2023-10-03 14:00:23 +0300

    Привязали MainWindow к ViewModel

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using WeatherDataSaver.ViewModels.Base;
+
+namespace WeatherDataSaver.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        #region Properties
+        private string _line = "This is MVVM string";
+        public string line
+        {
+            get => _line;
+            set => Set(ref _line, value);
+        }
+        #endregion
+    }
+}
