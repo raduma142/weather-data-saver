cadfdab9acf1b9b54121d702f54212c52c3d12b7
shosh <shoshin.142@ya.ru>
2023-10-06 03:38:39 +0300

    Добавили универсальную функцию генерации имени файла

--- Services/FileService/FileAccess.cs
+++ Services/FileService/FileAccess.cs
@@ -3,6 +3,7 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -19,31 +20,33 @@ namespace WeatherDataSaver.Services.FileService
         public string SaveDataSetJSON(ObservableCollection<DataRecord> dataSet, string path)
         {
             CheckPathExists(path);
-            return "";
+            string name = string.Format("Отчёт от {0} (c {1} по {2})", DateTime.Now.ToString("dd-MM-yyyy HH-mm-ss"), dataSet.First().date, dataSet.Last().date);
+            string reportPath = GenerateFileName(path, name, "json");
+
+            _logger.LogInformation($"Сохранён отчёт {reportPath}");
+
+            return reportPath;
         }
 
         //Сохранение данных в файл в формате JSON
         public string SaveDataSetXML(ObservableCollection<DataRecord> dataSet, string path)
         {
             CheckPathExists(path);
-            return "";
+            string name = string.Format("Отчёт от {0} (c {1} по {2})", DateTime.Now.ToString("dd-MM-yyyy HH-mm-ss"), dataSet.First().date, dataSet.Last().date);
+            string reportPath = GenerateFileName(path, name, "xml");
+
+            _logger.LogInformation($"Сохранён отчёт {reportPath}");
+
+            return reportPath;
         }
 
         //Сохранение данных в файл в формате CSV
         public string SaveDataSetCSV(ObservableCollection<DataRecord> dataSet, string path)
         {
             CheckPathExists(path);
-            string reportName = string.Format("Отчёт от {0} (c {1} по {2})", DateTime.Now.ToString("dd-MM-yyyy HH-mm-ss"), dataSet.First().date, dataSet.Last().date);
-            string reportFileName = reportName + ".csv";
-            string reportPath = Path.Combine(path, reportFileName);
 
-            //Проверка существования отчёта с таким именем
-            while (File.Exists(reportPath))
-            {
-                reportName += " (2)";
-                reportFileName = reportName + ".csv";
-                reportPath = Path.Combine(path, reportFileName);
-            }
+            string name = string.Format("Отчёт от {0} (c {1} по {2})", DateTime.Now.ToString("dd-MM-yyyy HH-mm-ss"), dataSet.First().date, dataSet.Last().date);
+            string reportPath = GenerateFileName(path, name, "csv");
 
             //Сохранение отчёта
             using (var file = new StreamWriter(reportPath, false, Encoding.Default))
@@ -75,6 +78,23 @@ namespace WeatherDataSaver.Services.FileService
             {
                 Directory.CreateDirectory(path);
             }
+        }
+
+        //Сформировать имя файла с учётом существубщих файлов
+        private string GenerateFileName(string path, string name, string format)
+        {
+            string reportFileName = name + "." + format;
+            string reportPath = Path.Combine(path, reportFileName);
+
+            //Проверка существования отчёта с таким именем
+            while (File.Exists(reportPath))
+            {
+                name += " (2)";
+                reportFileName = name + "." + format;
+                reportPath = Path.Combine(path, reportFileName);
+            }
+
+            return reportPath;
         }
 
         public FileAccess(ILogger<FileAccess> logger)
