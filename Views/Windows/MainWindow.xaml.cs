a3980a09bece3c143a628f5509f52587c152afa9
shosh <shoshin.142@ya.ru>
2023-10-07 15:37:51 +0300

    Добавили обработку вводимы клавиш в поле Температура

--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
 using WeatherDataSaver.Services.DataBaseService;
 using WeatherDataSaver.Services.FileService;
 using WeatherDataSaver.Services.ReportService;
@@ -9,6 +11,15 @@ namespace WeatherDataSaver
     public partial class MainWindow : Window
     {
         MainWindowViewModel viewModel;
+
+        List<Key> Keys = new List<Key>()
+        {
+            Key.D0, Key.D1, Key.D2, Key.D3, Key.D4, Key.D5, Key.D6, Key.D7, Key.D8, Key.D9,
+            Key.NumPad0, Key.NumPad1, Key.NumPad2, Key.NumPad3, Key.NumPad4,
+            Key.NumPad5, Key.NumPad6, Key.NumPad7, Key.NumPad8, Key.NumPad9,
+            Key.Separator, Key.OemPlus, Key.OemMinus, Key.Subtract, Key.Add,
+            Key.Decimal, Key.OemPeriod
+        };
         public MainWindow(IReportCreater reportCreater, IFileAccess fileAccess, IDataBaseAccess dataBaseAccess)
         {
             InitializeComponent();
@@ -16,6 +27,11 @@ namespace WeatherDataSaver
             viewModel.dataBaseAccess = dataBaseAccess;
             viewModel.reportCreater = reportCreater;
             viewModel.fileAccess = fileAccess;
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = Keys.IndexOf(e.Key) == -1;
         }
     }
 }
